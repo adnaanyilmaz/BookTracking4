15322b0e597bb532ebcbbc158755bfb4f14ebd2f
Adnan Yılmaz <adnaanyilmaz@gmail.com>
2024-10-19 17:35:59 +0300

    item_search_book.xml added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
+
 }
---
+++ app/src/main/java/com/example/booktracking4/domain/model/ui_model/search_model/Book.kt
@@ -0,0 +1,9 @@
+package com.example.booktracking4.domain.model.ui_model.search_model
+
+data class Book(
+    val title: String,
+    val authors: String,
+    val thumbnail: String,
+    val category: String
+)
+
