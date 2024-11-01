dd3ef5a825576e959d64a1c2a8e46b026648bae5
Adnan Yılmaz <keddemci.adnan@gmail.com>
2024-11-02 01:17:49 +0300

    book detail page designed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,13 @@ dependencies {
     implementation(libs.gson)
     implementation(libs.converter.gson)
 
+    //Room
+
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    ksp ("androidx.room:room-compiler:2.6.1")
+
+
     // Coil (Picassodan daha iyi çalışıyor bence. Bunu daha pratik kullanmakiçin bir extension yazdım)
     implementation(libs.coil)
 }
