80a60516b9dbd873caf55e0e5f24417fcb3553a4
Adnan Yılmaz <adnaanyilmaz@gmail.com>
2024-11-12 17:37:00 +0300

    .

--- app/src/main/java/com/example/booktracking4/data/remote/dto/detail_dto/İmageLinks.kt
+++
@@ -1,9 +0,0 @@
-package com.example.booktracking4.data.remote.dto.detail_dto
-
-data class İmageLinks(
-    val large: String,
-    val medium: String,
-    val small: String,
-    val smallThumbnail: String,
-    val thumbnail: String
-)
--- app/src/main/java/com/example/booktracking4/data/remote/dto/detail_dto/İndustryIdentifier.kt
+++
@@ -1,6 +0,0 @@
-package com.example.booktracking4.data.remote.dto.detail_dto
-
-data class İndustryIdentifier(
-    val identifier: String,
-    val type: String
-)
